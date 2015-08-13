4090ce4db1f189b9294c54b8f51714dc491fc4ef
InJung Chung <mu29gl@gmail.com>
2015-08-13 22:46:47 +0900

    Update Value class

--- value.h
+++ value.h
@@ -9,30 +9,54 @@
 #ifndef __lightjson__Value__
 #define __lightjson__Value__
 
-namespace lightjson {
+#include <string>
+#include <vector>
+using namespace std;
+
+namespace Json {
     class Value {
     private:
+        int intData;
+        string strData;
     public:
+        int type;
+        int& operator=(const int& data) {
+            type = 1;
+            intData = data;
+            return intData;
+        }
+        string& operator=(const string& data) {
+            type = 2;
+            strData = data;
+            return strData;
+        }
+        bool& operator=(const bool&);
+        Value& operator=(const Value&);
+        template <typename T>
+        vector<T>& operator=(const vector<T>&);
     };
     
     class NullValue : public Value {
-        
     };
     
     class IntValue : public Value {
-        
+    public:
+        Value& operator=(const Value& other);
     };
     
     class StringValue : public Value {
-        
+    public:
+        Value& operator=(const Value& other);
     };
     
     class BoolValue : public Value {
-        
+    public:
+        Value& operator=(const Value& other);
     };
     
     class ArrayValue : public Value {
-        
+    public:
+        Value& operator=(const Value& other);
     };
 }
 
