ece544a214f8f634e530fb4b20b20742f855dc08
InJung Chung <mu29gl@gmail.com>
2015-08-13 21:25:30 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lightjson
+//
+//  Created by 정인중 on 2015. 8. 13..
+//  Copyright (c) 2015년 InJung Chung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
