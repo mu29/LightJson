b9b32674de76bd704718b22909331f8fb7f4c2e1
InJung Chung <mu29gl@gmail.com>
2015-08-13 21:25:59 +0900

    Value class

---
+++ value.h
@@ -0,0 +1,43 @@
+//
+//  value.h
+//  lightjson
+//
+//  Created by 정인중 on 2015. 8. 13..
+//  Copyright (c) 2015년 InJung Chung. All rights reserved.
+//
+
+#ifndef __lightjson__Value__
+#define __lightjson__Value__
+
+namespace lightjson {
+    class Value {
+    private:
+    public:
+    };
+    
+    class NullValue {
+        
+    };
+    
+    class IntValue {
+        
+    };
+    
+    class StringValue {
+        
+    };
+    
+    class BoolValue {
+        
+    };
+    
+    class ArrayValue {
+        
+    };
+    
+    class ObjectValue {
+        
+    };
+}
+
+#endif /* defined(__lightjson__Value__) */
